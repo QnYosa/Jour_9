7212c58cc8e2bc51da5dbae8d56f13303686049f
QnYosa <55686931+QnYosa@users.noreply.github.com>
2019-10-04 09:25:47 +0200

    Update 00_hello.rb

--- 00_hello.rb
+++ 00_hello.rb
@@ -1,5 +1,16 @@
-def say_hello(first_name)
-   first_name = gets.chomp
-    puts "Hello #{first_name}!"
-end 
-say_hello(first_name)
+def say_hello (first_name)
+    puts "Bonjour #{first_name}"
+  end
+
+  def ask_first_name
+    puts "Bonjour quel est votre nom ? "
+    print "> "
+    first_name = gets.chomp
+    return first_name
+  end
+
+  def perform
+   first_name = ask_first_name
+    say_hello (first_name)
+  end
+perform
